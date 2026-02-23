6b4e7374ee12c18b8d1256e5c33c8bb4d28a6d37
arepa <135524116+arepaHub@users.noreply.github.com>
2026-02-23 14:36:23 -0400

    GUI UPDATE

--- Qreatorui.py
+++ Qreatorui.py
@@ -1,11 +1,17 @@
 import qrcode
 import customtkinter as ctk
-import pyglet
-pyglet.font.add_file("Minecraft.ttf")
+import pyperclip
 
 def qr_generator(link, save):
     link = qrcode.make(link)
     link.save(save)
+
+def close():
+    exit()
+    
+def pegar():
+    entrada_link.delete(0, "end")
+    entrada_link.insert(0, app.clipboard_get())
 
 
 def generar():
@@ -25,12 +31,12 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
-app.title("QReator")
+app.title("QReator by arepa")
 app.geometry("400x300")
 app.resizable(False, False)
 
-fuente = ctk.CTkFont(family="Minecraft", size=23)
-ctk.CTkLabel(app, text="QReator", font=fuente).pack()
+fuente = ctk.CTkFont(family="Arial", size=15)
+ctk.CTkLabel(app, text="QReator", font=fuente, text_color="white").pack(pady=10)
 
 entrada_link = ctk.CTkEntry(app, width=300,font=fuente, placeholder_text="Paste your link")
 entrada_link.pack(pady=10)
@@ -38,7 +44,9 @@ entrada_link.pack(pady=10)
 entrada_nombre = ctk.CTkEntry(app, width=300,font=fuente, placeholder_text="File Name")
 entrada_nombre.pack(pady=10)
 
-ctk.CTkButton(app, text="Create QR", font=fuente, width=200, command=generar).pack(pady=15)
+ctk.CTkButton(app, text="Create QR", font=fuente, width=200, command=generar).pack(pady=20)
+
+ctk.CTkButton(app, text="Exit", font=fuente, width=200, command=close).pack(pady=2)
 
 label_estado = ctk.CTkLabel(app, text="")
 label_estado.pack()
